184468cc428558408faa41b4580797c50805e0d1
R <ivan.js.style@gmail.com>
2021-05-25 22:16:48 +0600

    выполнил задачу. Тест засчитал. Так же выполнил с циклом while, но не стал добавлять.

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,16 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1
+  if (n === 0 || n === 1) {
+    return result
+  } else {
+    for (let i = 1; i <= n; i++) {
+      result = result * i
+    }
+    return result
+  }
 }
+
+factorial(0); // 1
+factorial(1); // 1
+factorial(3); // 6
+factorial(5); // 120
