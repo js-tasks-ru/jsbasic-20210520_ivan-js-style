3abc228ef66bf7af792986fe5bbc7f92cba92b71
R <ivan.js.style@gmail.com>
2021-05-26 10:56:34 +0600

    задача с проверкой на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,15 @@
 function checkSpam(str) {
-  // ваш код...
+  let reg = /1XBET|XXX/i
+  return reg.test(str.toUpperCase())
 }
+
+//либо более тривиальное, но тоже работает:
+// function checkSpam(str) {
+//   str = str.toUpperCase()
+//
+//   if (str.indexOf('XXX') !== -1 || str.indexOf('1XBET') !== -1) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
