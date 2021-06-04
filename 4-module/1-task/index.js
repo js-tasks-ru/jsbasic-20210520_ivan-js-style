431ab5253d566e410b54edc6cc1746f769e07e4d
R <ivan.js.style@gmail.com>
2021-06-04 10:11:26 +0600

    не понял формулировку задачи от слова совсем

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,20 @@
 function makeFriendsList(friends) {
   // ваш код...
+  const ul = document.createElement('ul')
+
+  ul.innerHTML = friends.map(item => `
+      <li>${item.firstName} ${item.lastName}</li>
+  `).join('')
+
+  return ul
 }
+
+// function makeFriendsList(friends) {
+//   let list = ''
+//   for (let item of friends) {
+//     list += `<li>${item.firstName} ${item.lastName}`
+//   }
+//   console.log(list)
+//   document.body.innerHTML = `<ul>${list}</ul>`
+//   return list
+// }
