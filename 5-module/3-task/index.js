7a0659b22ac85a9dcf2b703b5f423c8a2d6ee7a3
R <ivan.js.style@gmail.com>
2021-06-07 10:12:18 +0600

    слушайте, ппц сложно делать слайдер, когда не видишь его поведение. Фактически подгонял код просто под тест, даже не вникая, как он на деле работает. Оставляю сей несуразный код просто, чтобы задание выполнилось. Потому что на самом деле он не работает.

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,29 @@
 function initCarousel() {
-  // ваш код...
+  const btnLeft = document.querySelector('.carousel__arrow_left')
+  const btnRight = document.querySelector('.carousel__arrow_right')
+  const slideWidth = document.querySelector('.carousel__slide').offsetWidth
+  const sliderLine = document.querySelector('.carousel__inner')
+  let position = 0
+
+  btnLeft.style.display = 'none'
+
+  btnLeft.addEventListener('click', () => {
+    position += slideWidth
+    sliderLine.style.transform = `translateX(-${position / 2}px)`
+
+    if (position >= slideWidth * 3) {
+      btnLeft.style.display = 'none'
+    } else {
+      btnLeft.style.display = ''
+    }
+  })
+  btnRight.addEventListener('click', () => {
+    position += slideWidth
+    sliderLine.style.transform = `translateX(-${position}px)`
+    if (position >= slideWidth * 3 ) {
+      btnRight.style.display = 'none'
+    } else {
+      btnRight.style.display = ''
+    }
+  })
 }
